e3c1e22a49f745465a86464cbe05821b618e6405
rachelyvernes <rachel.yvernes@gmail.com>
2026-05-21 11:37:57 +0200

    Update Header.vue

--- front/components/Header.vue
+++ front/components/Header.vue
@@ -149,19 +149,19 @@
 
     <!-- COLONNE DROITE : TES AUTRES CHOSES -->
     <aside class="space-y-4">
-      <p class="-h4 pb-4 text-white">Ecrivez moi ! Je vous réponds sous 48h. Ou prenez rendez-vous directement ici :</p>
+      <p class="-h4 pb-4 text-white">Ecrivez moi ou prenez rendez-vous directement ici :</p>
 
       <div class="p-4 rounded-base border border-default bg-neutral-primary-soft bg-white text-black text-center">
         <a class="" href="https://consultation.avocat.fr/consultation-cabinet/forms.php?hashid=47801a343db3450c2b78" target="_blank" rel="noreferrer">
         <p class="font-medium mb-2 text-black flex items-center justify-center gap-2"><img src="/img/location-dotblack.svg" alt="" class="w-6 h-6 shrink-0"> Rendez-vous au cabinet</p>
-          <p class="text-sm text-black">1h 100 € HT</p>
+          <p class="text-sm text-black">1h 150 € HT</p>
         </a>
       </div>
 
       <div class="p-4 rounded-base border border-default bg-neutral-primary-soft bg-transparent text-black text-center">
       <a class="" href="https://consultation.avocat.fr/consultation-video/forms.php?hashid=b2374ea43e86b39d23c3" target="_blank" rel="noreferrer">
         <p class="font-medium mb-2 text-white flex items-center justify-center gap-2"><img src="/img/calendarwhite.svg" alt="" class="w-6 h-6 shrink-0"> Rendez-vous en visioconférence</p>
-        <p class="text-sm text-white">30min - 50 € HT</p>
+        <p class="text-sm text-white">30min - 90 € HT</p>
       </a>
       </div>
               <p class="pt-4 text-base text-white">Cette somme sera déduite des honoraires si le rendez-vous est suivi d’une ouverture du dossier au cabinet.</p>
